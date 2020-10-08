888706058ba4e48d9277aa68cb43e4e3e62a809b
BeirlaenAaron <55881661+BeirlaenAaron@users.noreply.github.com>
2020-10-08 16:12:22 +0200

    Labo03

--- script/app.js
+++ script/app.js
@@ -1,13 +1,20 @@
-function handleFloatingLabel() {
-   
-}
+function handleFloatingLabel() {}
 
 function handlePasswordSwitcher() {
-    
+  let input = document.getElementById('password');
+  let toggle = document.getElementById('togglePassword');
+
+  toggle.addEventListener('click', function () {
+    if (input.type === 'password') {
+      input.type = 'text';
+    } else {
+      input.type = 'password';
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log('Script loaded!');
-    handleFloatingLabel();
-    handlePasswordSwitcher();
+  console.log('Script loaded!');
+  handleFloatingLabel();
+  handlePasswordSwitcher();
 });
